997fd0873a3051648082ffa4ecde9a83a2d61a62
unknownJoe24 <56737384+unknownJoe24@users.noreply.github.com>
2021-10-25 11:59:41 -0400

    Basic controller implementation

---
+++ projectGlitch/Assets/Scripts/playerControls.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControls : MonoBehaviour
+{
+
+    //public Rigidbody rigidbody = GetComponent<Rigidbody>;
+    public float speed = 5.0f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rigidbody = GetComponent<Rigidbody>;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift)){
+            speed = 10.0f;
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftShift)){
+            speed = 5.0f;
+        }
+
+        Movement();
+    }
+
+    public void Movement(){
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //float verticalInput = Input.GetAxis("Vertical");
+        float jumpInput = Input.GetAxis("Jump");
+
+        transform.Translate(new Vector2(horizontalInput, jumpInput) * speed * Time.deltaTime);
+    }
+}
