aa3e0d8ae749492ccb73447d6eff48fac50c70c3
unknownJoe24 <56737384+unknownJoe24@users.noreply.github.com>
2021-12-05 22:28:15 -0500

    Finished level one and started work on the main menu

---
+++ projectGlitch/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class mainMenu : MonoBehaviour
+{
+    public Button startGame;
+    public Button options;
+    public Button endGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
