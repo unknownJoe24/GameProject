e7c8ea995c6e68b1e31a347c8e477362daa128a5
unknownJoe24 <56737384+unknownJoe24@users.noreply.github.com>
2021-12-11 12:47:44 -0500

    Added enemy and started enemy script

---
+++ projectGlitch/Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyMovement : MonoBehaviour
+{
+
+    public GameObject player; //get player object to track
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get player object from tag
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
