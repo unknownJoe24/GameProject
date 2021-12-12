7760c011a87670e2797a3b86c68f4626b51467c2
unknownJoe24 <56737384+unknownJoe24@users.noreply.github.com>
2021-12-11 21:13:20 -0500

    Small  work on enemy movement

--- projectGlitch/Assets/Scripts/enemyMovement.cs
+++ projectGlitch/Assets/Scripts/enemyMovement.cs
@@ -6,18 +6,21 @@ using UnityEngine.AI;
 public class enemyMovement : MonoBehaviour
 {
 
-    public GameObject player; //get player object to track
+    Transform player; //get player object to track
+    NavMeshAgent nav;
 
     // Start is called before the first frame update
     void Start()
     {
         //get player object from tag
-
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //get navmesh
+        nav = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        nav.SetDestination(player.position);
     }
 }
